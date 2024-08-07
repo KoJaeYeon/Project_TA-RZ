cf92db930daa3432073c460e02972e65ad6aa652
고재연 <kojy2004@yonsei.ac.kr>
2024-08-07 13:52:03 +0900

    Init Assets & Plugins
    Plugin
    - Newtonsoft Json
    - Cinemachine
    - Input System
    - Oculus XR Plugin
    - XR Interaction Toolkit
    - XR Plugin Management
    Asset
    - Behavior Designer
    - Extenject Dependency Injection IOC

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // 한글테스트
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
