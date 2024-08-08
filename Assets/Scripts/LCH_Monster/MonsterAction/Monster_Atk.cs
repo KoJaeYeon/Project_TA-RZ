16c63b01b7ea45e849775a32f4088daf4bbfa91d
이창헌 <amckdgjs1@gmail.com>
2024-08-08 18:19:02 +0900

    [Feat]Monster 타입 정의

--- Assets/Scripts/LCH_Monster/MonsterAction/Monster_Atk.cs
+++ Assets/Scripts/LCH_Monster/MonsterAction/Monster_Atk.cs
@@ -1,3 +1,4 @@
+using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,10 +7,17 @@ using UnityEngine;
 [TaskCategory("Monster/General")]
 public class Monster_Atk : Action
 {
-    [SerializeField] Animator animator; 
+    [SerializeField] Animator animator;
+    [SerializeField] SharedFloat AtkSpeed;
     public override TaskStatus OnUpdate()
     {
-
+        StartCoroutine(WaitForNextAtk());
         return TaskStatus.Success;
+    }
+
+    IEnumerator WaitForNextAtk()
+    {
+        Debug.Log("ㅎㅇ");
+        yield return new WaitForSeconds(AtkSpeed.Value);
     }
 }
