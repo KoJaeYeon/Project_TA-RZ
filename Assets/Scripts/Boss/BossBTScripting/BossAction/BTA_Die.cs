c50d941430d9a6989eb2c945aace872334c5af93
왕성민 <wsm6753@gmail.com>
2024-08-09 15:30:02 +0900

    [Add] 보스 Die ActionTask 추가

---
+++ Assets/Scripts/Boss/BossBTScripting/BossAction/BTA_Die.cs
@@ -0,0 +1,19 @@
+using BehaviorDesigner.Runtime.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTA_Die : BossAction
+{
+    private readonly int _hashDie = Animator.StringToHash("Die");
+
+    public override void OnStart()
+    {
+        _anim.SetTrigger(_hashDie);
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        return TaskStatus.Success;
+    }
+}
