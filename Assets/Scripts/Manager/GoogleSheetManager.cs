94f514a78bd2ad392ae206a6237e50dee609191f
고재연 <kojy2004@yonsei.ac.kr>
2024-08-09 15:15:56 +0900

    [docs] WebCode 추가

--- Assets/Scripts/Manager/GoogleSheetManager.cs
+++ Assets/Scripts/Manager/GoogleSheetManager.cs
@@ -15,3 +15,29 @@ public class GoogleSheetManager : MonoBehaviour
         print(data);
     }
 }
+
+#region WebCode
+/*
+function doGet(e) {
+  var sheet = SpreadsheetApp.getActive();
+  var nse = sheet.getSheetByName('시트2');
+  var data = [];
+  var rlen = nse.getLastRow();
+  var clen = nse.getLastColumn();
+  var rows = nse.getRange(1, 1, rlen, clen).getValues();
+  for (var i = 0; i < rows.length; i++) {
+    var datarow = rows[i];
+    var record = {};
+    for (var j = 1; j < clen; j++) {
+      record[rows[0][j]] = datarow[j];
+    }
+    data.push(record);
+  }
+  console.log(data);
+  var result = JSON.stringify(data);
+  return ContentService.createTextOutput(result).setMimeType(ContentService.MimeType.JSON);
+}
+
+
+ */
+#endregion
