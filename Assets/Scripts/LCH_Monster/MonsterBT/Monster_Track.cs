267bd52647d50c9fd6cf4c3c81c1b661907e5106
이창헌 <amckdgjs1@gmail.com>
2024-08-12 10:57:23 +0900

    [Feat]NavMesh로 움직이도록 수정중

--- Assets/Scripts/LCH_Monster/MonsterBT/Monster_Track.cs
+++ Assets/Scripts/LCH_Monster/MonsterBT/Monster_Track.cs
@@ -3,23 +3,29 @@ using BehaviorDesigner.Runtime.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 [TaskCategory("Monster/General")]
 public class Monster_Track : Action
 {
     [SerializeField] SharedTransform TargetTransform;
     [SerializeField] SharedFloat MoveSpeed;
+    [SerializeField] NavMeshAgent Nav;
     public override TaskStatus OnUpdate()
     {
         if(TargetTransform == null)
         {
             return TaskStatus.Failure;
         }
-
+        //if (Nav == null)
+        //{
+        //    Nav = GetComponent<NavMeshAgent>();
+        //}
         Vector3 direction = (TargetTransform.Value.position - Owner.transform.position).normalized;
 
         if (TargetTransform != null)
         {
-            Owner.transform.position += direction * MoveSpeed.Value * Time.deltaTime;
+            Nav.SetDestination(TargetTransform.Value.position);
+            //Owner.transform.position += direction * MoveSpeed.Value * Time.deltaTime;
             return TaskStatus.Success;
         } 
         else return TaskStatus.Failure;
