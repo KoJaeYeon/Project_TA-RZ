0e800bab01a1b60e1f051a235ac1d1dea2afc6b6
이창헌 <amckdgjs1@gmail.com>
2024-08-16 11:01:00 +0900

    [Feat]일반 몬스터 데미지 받았을 경우 bt 추가

--- Assets/Scripts/LCH_Monster/Monster.cs
+++ Assets/Scripts/LCH_Monster/Monster.cs
@@ -17,17 +17,24 @@ public class Monster : MonoBehaviour,IHit
     public float Mon_Common_MovementSpeed;
     public float Mon_Common_CoolTime;
 
+    public bool isDamaged;
+    public float Mon_Common_CurrentHp;
     [Inject] public Player Player { get;}
 
     void Start()
     {
+        Mon_Common_CurrentHp = Mon_Common_Stat_Hp;
         Bt = GetComponent<BehaviorTree>();
     }
 
     public void Hit(float damage)
     {
-        Mon_Common_Stat_Hp-=damage;
-        if (Mon_Common_Stat_Hp <= 0)
+        isDamaged = true;
+        Mon_Common_CurrentHp -= damage;
+        StartCoroutine(WaitForStun());
+        var anim = GetComponent<Animator>();
+        anim.SetTrigger("Damaged");
+        if (Mon_Common_CurrentHp <= 0)
         {
 
         }
@@ -39,6 +46,18 @@ public class Monster : MonoBehaviour,IHit
 
     private void OnTriggerEnter(Collider other)
     {
-        //if(other.CompareTag())
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("데미지받음");
+            Hit(10);
+        }
+    }
+
+    public IEnumerator WaitForStun()
+    {
+        Bt.enabled = false;
+        yield return new WaitForSeconds(5);
+        Bt.enabled = true;
+        isDamaged = false;
     }
 }
--- Assets/Scripts/LCH_Monster/MonsterBT/Monster_CheckDamaged.cs
+++ Assets/Scripts/LCH_Monster/MonsterBT/Monster_CheckDamaged.cs
@@ -2,17 +2,20 @@ using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils.Editor.BuildingBlocks;
 using UnityEngine;
 [TaskCategory("Monster/General")]
-public class Monster_CheckHp : Conditional
+public class Monster_CheckDamaged : Conditional
 {
     [SerializeField] SharedMonster Monster;
+    // Start is called before the first frame update
     public override TaskStatus OnUpdate()
     {
-        if (Monster.Value.Mon_Common_CurrentHp <= 0)
+        if (Monster.Value.isDamaged == true)
         {
-            return TaskStatus.Success;
+            return TaskStatus.Failure;
         }
-        return TaskStatus.Failure;
+        return TaskStatus.Success;
+
     }
 }
