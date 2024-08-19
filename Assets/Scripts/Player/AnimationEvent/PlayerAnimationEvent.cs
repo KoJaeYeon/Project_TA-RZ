e8bc3f1a97fc684a347502dd2653f8cb2e5699db
junhyungs <jin05003@naver.com>
2024-08-19 15:14:11 +0900

    [fix] 네 번째 기본공격 이펙트 추가

--- Assets/Scripts/Player/AnimationEvent/PlayerAnimationEvent.cs
+++ Assets/Scripts/Player/AnimationEvent/PlayerAnimationEvent.cs
@@ -19,7 +19,7 @@ public class PlayerAnimationEvent : MonoBehaviour
     private PlayerEffect _effect;
     private Dictionary<AttackType, Action> _attackDictionary = new Dictionary<AttackType, Action>();
 
-    private WaitForSeconds _returnTime = new WaitForSeconds(0.5f);
+    
 
     private void Start()
     {
@@ -45,52 +45,21 @@ public class PlayerAnimationEvent : MonoBehaviour
     //첫 번째 공격 이펙트
     public void FirstAttack()
     {
-        GameObject firstEffect = _effect.GetAttackEffect(AttackType.firstAttack);
-        ParticleSystem effectParticle = firstEffect.GetComponent<ParticleSystem>();
-
-        firstEffect.transform.localPosition = Vector3.zero;
-        firstEffect.transform.localRotation = Quaternion.identity;
-        firstEffect.transform.parent = null;
-        effectParticle.Play();
-
-        StartCoroutine(ReturnFirstEffect(firstEffect));
-
+       _effect.Active_FirstEffect();
         _attackDictionary[AttackType.firstAttack].Invoke();
     }
 
     //두 번째 공격 이펙트
     public void SecondAttack()
     {
-        GameObject secondEffect = _effect.GetAttackEffect(AttackType.secondAttack);
-        ParticleSystem effectParticle = secondEffect.GetComponent<ParticleSystem>();
-
-        secondEffect.transform.localPosition = Vector3.zero;
-        secondEffect.transform.localRotation = Quaternion.identity;
-        secondEffect.transform.parent = null;
-        effectParticle.Play();
-
-        StartCoroutine(ReturnSecondEffect(secondEffect));   
-
+        _effect.Active_SecondEffect();
         _attackDictionary[AttackType.secondAttack].Invoke();
     }
 
     //세 번째 공격 이펙트
     public void ThirdAttack()
     {
-        GameObject thirdEffect = _effect.GetAttackEffect(AttackType.thirdAttack);
-        ParticleSystem effectParticle = thirdEffect.GetComponent<ParticleSystem>();
-
-        thirdEffect.transform.localPosition = Vector3.zero;
-
-        thirdEffect.transform.localRotation = Quaternion.Euler(-90f, 0f, 0f);
-
-
-
-        thirdEffect.transform.parent = null;
-        effectParticle.Play();
-
-        StartCoroutine(ReturnThirdEffect(thirdEffect)); 
-
+        _effect.Active_ThirdEffect();
         _attackDictionary[AttackType.thirdAttack].Invoke();
     }
 
@@ -98,28 +67,6 @@ public class PlayerAnimationEvent : MonoBehaviour
     public void FourthAttack()
     {
         _attackDictionary[AttackType.fourthAttack].Invoke();
-    }
-
-    //종료
-    private IEnumerator ReturnFirstEffect(GameObject effect)
-    {
-        yield return _returnTime;
-
-        effect.transform.SetParent(_effect.GetAttackEffectTransform(AttackType.firstAttack));
-    }
-
-    private IEnumerator ReturnSecondEffect(GameObject effect)
-    {
-        yield return _returnTime;
-
-        effect.transform.SetParent(_effect.GetAttackEffectTransform(AttackType.secondAttack));
-    }
-
-    private IEnumerator ReturnThirdEffect(GameObject effect)
-    {
-        yield return _returnTime;
-
-        effect.transform.SetParent(_effect.GetAttackEffectTransform(AttackType.thirdAttack));
     }
 
     public void SkillEnd()
