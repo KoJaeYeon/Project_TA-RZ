7aa5d918d1ccbe0a8dc16c1d943b73209c641527
왕성민 <wsm6753@gmail.com>
2024-08-21 18:05:53 +0900

    [Add] 뿌리의 상태를 확인하는 컨디셔널 노드 추가

---
+++ Assets/Scripts/Boss/Root/BTC_Root_CheckState.cs
@@ -0,0 +1,23 @@
+using BehaviorDesigner.Runtime.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[TaskCategory("Root")]
+public class BTC_Root_CheckState : Conditional
+{
+    [SerializeField] private RootContoller _owner;
+
+    public override void OnAwake()
+    {
+        _owner = GetComponent<RootContoller>();
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        if (_owner.rootState == RootState.Hide) return TaskStatus.Success; 
+
+        return TaskStatus.Failure;
+    }
+}
