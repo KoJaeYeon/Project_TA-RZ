9d61f7080a5edd4c27cf41540fc03cd17c0e322d
왕성민 <wsm6753@gmail.com>
2024-08-23 11:31:01 +0900

    [Add] 보스 기믹 액션 노드 추가

---
+++ Assets/Scripts/Boss/BossBTScripting/BossAction/BTA_Boss_MarkGimmick.cs
@@ -0,0 +1,22 @@
+using BehaviorDesigner.Runtime.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[TaskCategory("Boss/Gimmick")]
+public class BTA_Boss_MarkGimmick : BossAction
+{
+    private readonly int _hashGimmickReady = Animator.StringToHash("GimmickReady");
+
+    public override void OnStart()
+    {
+        _anim.SetTrigger(_hashGimmickReady);
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        _owner.MarkGimmick();
+
+        return TaskStatus.Success;
+    }
+}
