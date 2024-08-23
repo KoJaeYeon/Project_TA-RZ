05994514c761c77c417019d5d59347e795e5d6d5
고재연 <kojy2004@yonsei.ac.kr>
2024-08-23 10:32:02 +0900

    [fix] 플레이어 로드방식 수정

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -308,29 +308,22 @@ public class Player : MonoBehaviour, IHit
 
     IEnumerator LoadStat()
     {
-        while (true)
-        {
-            var stat = dataManager.GetStat("P101") as PC_Common_Stat;
-            if (stat == null)
-            {
-                Debug.Log("Player의 스탯을 받아오지 못했습니다.");
-                yield return new WaitForSeconds(1f);
-            }
-            else
-            {
-                _playerStat = stat;
-                Debug.Log("Player의 스탯을 성공적으로 받아왔습니다.");
-                CurrentAtk = _playerStat.Atk_Power;                
-                CurrentHP = _playerStat.HP;
-                HP = _playerStat.HP;
-                CurrentStamina = 100;
-                CurrentSkill = 0;
-                CurrentAmmo = 0;
-                OnPropertyChanged(nameof(stat.Resource_Own_Num));
-                yield break;
-            }
+        yield return new WaitWhile(() => {
+            Debug.Log("Player의 데이터를 받아오는 중입니다.");
+            return dataManager.GetStat("P101") == null;
+        });
 
-        }
+        var stat = dataManager.GetStat("P101") as PC_Common_Stat;
+        _playerStat = stat;
+        Debug.Log("Player의 스탯을 성공적으로 받아왔습니다.");
+        CurrentAtk = _playerStat.Atk_Power;
+        CurrentHP = _playerStat.HP;
+        HP = _playerStat.HP;
+        CurrentStamina = 100;
+        CurrentSkill = 0;
+        CurrentAmmo = 0;
+        OnPropertyChanged(nameof(stat.Resource_Own_Num));
+        yield break;
     }
 
     IEnumerator LoadSkillCounsumption()
