f4c98bb2c6dc46da6ea80ba980b1fe400f142103
왕성민 <wsm6753@gmail.com>
2024-08-23 11:32:05 +0900

    [fix] 기믹 컨트롤러 수정

--- Assets/Scripts/Boss/Gimmick/GimmickController.cs
+++ Assets/Scripts/Boss/Gimmick/GimmickController.cs
@@ -10,25 +10,69 @@ public class GimmickController : MonoBehaviour
     [SerializeField] private float _downValue;
     [SerializeField] private float _upValue;
 
-    [SerializeField] private ProBuilderMesh m_Editor;
+    private bool _isHurt;
+
+    private Vector3 _scale;
+    private Coroutine _hurtCoroutine;
 
     private void Awake()
     {
-        m_Editor = GetComponent<ProBuilderMesh>();
+        _scale = transform.localScale;
+    }
+
+    private void OnEnable()
+    {
+        transform.localScale = _scale;
     }
 
     private void Update()
     {
-        Test();
+        if (!_isHurt) OnActivateNonCombat();
     }
 
-    private void Test()
+    public void OnActivateCombat()
     {
-        if (m_Editor.transform.localScale.x >= _radius) return;
+        if (transform.localScale.x <= _downValue) ChangedScale(false, transform.localScale.x);
+        else ChangedScale(false, _downValue);
 
-        Vector3 scale = m_Editor.transform.localScale;
-        scale.x += Time.deltaTime;
-        scale.z += Time.deltaTime;
-        m_Editor.transform.localScale = scale;
+        _isHurt = true;
+
+        if (_hurtCoroutine != null)
+        {
+            StopCoroutine(_hurtCoroutine);
+        }
+
+        _hurtCoroutine = StartCoroutine(CoDelayTime());
+    }
+
+    private void OnActivateNonCombat()
+    {
+        if (transform.localScale.x >= _radius) return;
+
+        ChangedScale(true, _upValue);
+    }
+
+    private void ChangedScale(bool updown, float changedScale)
+    {
+        Vector3 scale = transform.localScale;
+        switch (updown)
+        {
+            case true:          
+                scale.x += changedScale * Time.deltaTime;
+                scale.z += changedScale * Time.deltaTime;
+                transform.localScale = scale;
+                break;
+            case false:
+                scale.x -= changedScale;
+                scale.z -= changedScale;
+                transform.localScale = scale;
+                break;
+        }
+    }
+
+    private IEnumerator CoDelayTime()
+    {
+        yield return new WaitForSeconds(1f);
+        _isHurt = false;
     }
 }
