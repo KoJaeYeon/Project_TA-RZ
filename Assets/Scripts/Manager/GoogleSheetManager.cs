bd6877064ffb47b1805e34e1df824a8ec7a2f559
KoJaeYeon <kojy2004@yonsei.ac.kr>
2024-08-26 12:07:32 +0900

    [feat] 스테이지 및 몬스터 타입 밸류 파싱

--- Assets/Scripts/Manager/GoogleSheetManager.cs
+++ Assets/Scripts/Manager/GoogleSheetManager.cs
@@ -13,6 +13,10 @@ public class GoogleSheetManager : MonoBehaviour
     const string _PC_Attack_URL = "https://script.google.com/macros/s/AKfycbxRtDJSB7VeIkbcIeKxRJoBsDELpf4z9yDR2rlAVxILUH2Klk4UmCZt_p7KfuU9DyCp9g/exec";
     const string _PC_Melee_URL = "https://script.google.com/macros/s/AKfycbyn9y5T39Pu3D_aNJmjO_UpBPyJcL3U7jcvmN92shIOLghxTY5HJL5_k88YhxhUG2HQ/exec";
     const string _Monster_Stat_URL = "https://script.google.com/macros/s/AKfycbzpl1LyCn504Guowz3d5ttyCyWPzqfhWvgNhYVg6kby4loIIf8Triar3JWCaxHh42be/exec";
+    const string _Monster_Ability = "https://script.google.com/macros/s/AKfycbxpGKWuflt68ZnuXWpqoYMQM_3-gr6d-HtHLYmVLS9ZnBU4xjhO3ysaiK_hrs6tNwQ1/exec";
+    const string _Map_Stat_URL = "https://script.google.com/macros/s/AKfycbz1OvZKBC5yz-p_UeX9163KMK0T5FrcAdlBHLxn-XJZzQ7YYZMM7TX5Jh0C_KaIDaf2/exec";
+
+    
     string data = string.Empty;
 
     [SerializeField] bool TryConnectSheet;
@@ -30,6 +34,8 @@ public class GoogleSheetManager : MonoBehaviour
             StartCoroutine(SaveJsonData(nameof(_PC_Attack_URL), _PC_Attack_URL));
             StartCoroutine(SaveJsonData(nameof(_PC_Melee_URL), _PC_Melee_URL));
             StartCoroutine(SaveJsonData(nameof(_Monster_Stat_URL), _Monster_Stat_URL));
+            StartCoroutine(SaveJsonData(nameof(_Monster_Ability), _Monster_Ability));
+            StartCoroutine(SaveJsonData(nameof(_Map_Stat_URL), _Map_Stat_URL));
             return;
         }
 
@@ -41,6 +47,8 @@ public class GoogleSheetManager : MonoBehaviour
             RequestJsonRead(nameof(_PC_Attack_URL));
             RequestJsonRead(nameof(_PC_Melee_URL));
             RequestJsonRead(nameof(_Monster_Stat_URL));
+            RequestJsonRead(nameof(_Monster_Ability));
+            RequestJsonRead(nameof(_Map_Stat_URL));
         }
         else
         {
@@ -50,6 +58,8 @@ public class GoogleSheetManager : MonoBehaviour
             StartCoroutine(RequestSJsonAPI(nameof(_PC_Attack_URL), _PC_Attack_URL));
             StartCoroutine(RequestSJsonAPI(nameof(_PC_Melee_URL), _PC_Melee_URL));
             StartCoroutine(RequestSJsonAPI(nameof(_Monster_Stat_URL), _Monster_Stat_URL));
+            StartCoroutine(RequestSJsonAPI(nameof(_Monster_Ability), _Monster_Ability));
+            StartCoroutine(RequestSJsonAPI(nameof(_Map_Stat_URL), _Map_Stat_URL));
         }
     }
     private void Update()
