8d03d3955bb9252c0710fe63c44e6f3acb4aefdc
왕성민 <wsm6753@gmail.com>
2024-08-26 11:27:19 +0900

    [fix] 뿌리가 Hide 상태일 때 계속해서 떨어지는 문제 수정

--- Assets/Scripts/Boss/Root/BTA_Root_Hide.cs
+++ Assets/Scripts/Boss/Root/BTA_Root_Hide.cs
@@ -9,34 +9,16 @@ public class BTA_Root_Hide : Action
     [SerializeField] private RootContoller _owner;
     private float _range = 10f;
 
-    private Vector3 _startPos;
-    Vector3 direction;
-
-    private bool _isDash = false;
-
     public override void OnAwake()
     {
         _owner = GetComponent<RootContoller>();
     }
 
-    public override void OnStart()
-    {
-        _startPos = transform.position;
-        _isDash = true;
-    }
-
     public override TaskStatus OnUpdate()
     {
-        _owner.transform.Translate(Vector3.down * Time.deltaTime);
+        if (_owner.transform.position.y > -_range)
+            _owner.transform.Translate(Vector3.down * Time.deltaTime);
 
-        float distance = Vector3.Distance(transform.position, _startPos);
-
-        if (distance >= _range)
-        {
-            _isDash = false;
-            return TaskStatus.Success;
-        }
-
-        return TaskStatus.Running;
+        return TaskStatus.Success;
     }
 }
