34931a44a73f7df7d39837ee08b27bf3a0127e01
KoJaeYeon <kojy2004@yonsei.ac.kr>
2024-08-26 17:01:20 +0900

    [feat] String데이터 파싱

---
+++ Assets/Scripts/UI/StringData.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class StringData : MonoBehaviour
+{
+    [Inject]
+    DataManager _dataManager;
+    [SerializeField] string IDStr;
+    private void Awake()
+    {
+        StartCoroutine(LoadString());
+    }
+
+    IEnumerator LoadString()
+    {
+        yield return new WaitWhile(() => (_dataManager.GetString(IDStr).Equals(string.Empty)));
+        var tmp = GetComponent<TextMeshProUGUI>();
+        if(tmp != null)
+        {
+            tmp.text = _dataManager.GetString(IDStr);
+            Destroy(this);
+        }
+        else
+        {
+           var tmpT = GetComponent<Text>();
+            tmpT.text = _dataManager.GetString(IDStr);
+            Destroy(this);
+        }
+
+    }
+}
