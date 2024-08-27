28d63f9536ac7fd9c39afa63358e8800d3a1ae0d
KoJaeYeon <kojy2004@yonsei.ac.kr>
2024-08-27 13:31:20 +0900

    [feat] 상자 상호작용 기능 추가

--- Assets/Scripts/Player/PlayerInteractionTrigger.cs
+++ Assets/Scripts/Player/PlayerInteractionTrigger.cs
@@ -1,11 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class PlayerInteractionTrigger : MonoBehaviour
 {
+    [Inject]
+    Player _player;
     private void OnTriggerEnter(Collider other)
     {
-        //other.GetComponent<>
+        var iInter = other.GetComponent<IInteractable>();
+        if(iInter == null )
+        {
+            return;
+        }
+
+        _player.Interactable = iInter;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        var iInter = other.GetComponent<IInteractable>();
+        if (iInter == null)
+        {
+            return;
+        }
+        else if(iInter == _player.Interactable)
+        {
+            _player.Interactable = null;
+        }        
     }
 }
