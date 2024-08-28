e1224116cd1b5129b97ee5d90076533f0d0f0848
고재연 <kojy2004@yonsei.ac.kr>
2024-08-28 20:10:39 +0900

    [feat] 데미지 텍스트 오브젝트 풀링

--- Assets/Scripts/Manager/PoolContainer.cs
+++ Assets/Scripts/Manager/PoolContainer.cs
@@ -5,10 +5,15 @@ public class PoolContainer : MonoInstaller
 {
     [SerializeField]
     private GameObject _poolManager;
+    [SerializeField]
+    private GameObject UIPoolManager;
 
     public override void InstallBindings()
     {
-            var managerInstance = Container.InstantiatePrefabForComponent<PoolManager>(_poolManager);
-            Container.BindInstance(managerInstance).AsSingle().NonLazy();
+        var managerInstance = Container.InstantiatePrefabForComponent<PoolManager>(_poolManager);
+        Container.BindInstance(managerInstance).AsSingle().NonLazy();
+
+        var UImanagerInstance = Container.InstantiatePrefabForComponent<UIPoolManager>(UIPoolManager);
+        Container.BindInstance(UImanagerInstance).AsSingle().NonLazy();
     }
 }
