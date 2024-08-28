94b9f90aba4543e0c9b3d7bd1781e6ce38f305e2
이창헌 <amckdgjs1@gmail.com>
2024-08-28 18:45:17 +0900

    [Feat]몬스터C 애니메이션 추가

---
+++ Assets/Monster_C.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster_C : Monster
+{
+    // Start is called before the first frame update
+    protected override void Awake()
+    {
+        base.Awake();
+        idStr = "E103";
+    }
+
+
+}
