9becf2caf11b1f342efcfc91375aa7d63a9e2a0f
왕성민 <wsm6753@gmail.com>
2024-08-29 17:08:08 +0900

    [fix] Player를 바라보는 메서드 수정

--- Assets/Scripts/Boss/BossBTScripting/BossAction/BTA_Boss_LookPlayer.cs
+++ Assets/Scripts/Boss/BossBTScripting/BossAction/BTA_Boss_LookPlayer.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 
 public class BTA_Boss_LookPlayer : BossAction
 {
+    private Quaternion _rot;
+
+    public override void OnStart()
+    {
+        _rot = _owner.PlayerRot();
+    }
+
     public override TaskStatus OnUpdate()
     {
-        _owner.LookAtPlayer();
+        _owner.LookAtPlayer(_rot);
 
-        float angle = Quaternion.Angle(transform.rotation, _owner.PlayerRot());
+        float angle = Quaternion.Angle(transform.rotation, _rot);
        
         if (angle < 0.1f)
         { 
