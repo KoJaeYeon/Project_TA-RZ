4950de77bafafd9b08f459bdc108bf6b2d054370
KoJaeYeon <kojy2004@yonsei.ac.kr>
2024-09-02 14:59:36 +0900

    [feat] OutGameUI 선택 및 취소 연동

--- Assets/EnableChoiceUI.cs
+++ Assets/EnableChoiceUI.cs
@@ -1,27 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class EnableChoiceUI : MonoBehaviour
 {
     [SerializeField] private GameObject _ui;
-    static GameObject SelectedChoiceButton;
 
     private void OnEnable()
     {
         EventSystem.current.SetSelectedGameObject(_ui);
-    }
-
-    private void Update()
-    {
-        if (EventSystem.current.currentSelectedGameObject == null)
-        {
-            EventSystem.current.SetSelectedGameObject(SelectedChoiceButton);
-        }
-        else
-        {
-            SelectedChoiceButton = EventSystem.current.currentSelectedGameObject;
-        }
     }
 }
---
+++ Assets/Scripts/UI/OutGameUI.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OutGameUI : MonoBehaviour
+{
+    [SerializeField] InputActionReference cancelAction;
+    [SerializeField] GameObject[] ChoiceBox;
+
+    void OnEnable()
+    {
+        cancelAction.action.Enable();
+        cancelAction.action.performed += OnCancel;
+    }
+
+    void OnDisable()
+    {
+        cancelAction.action.performed -= OnCancel;
+        cancelAction.action.Disable();
+    }
+
+    public void OnSubmit_Continue()
+    {
+        ChoiceBox[0].SetActive(false);
+        ChoiceBox[1].SetActive(true);
+    }
+    public void OnSubmit_NewGame()
+    {
+        ChoiceBox[0].SetActive(false);
+        ChoiceBox[2].SetActive(true);
+    }
+
+    public void OnSubmit_Quit()
+    {
+        Application.Quit();
+    }
+
+    private void OnCancel(InputAction.CallbackContext context)
+    {
+        Debug.Log("Cancel");
+        if (ChoiceBox[1].activeSelf)
+        {
+            // 메뉴가 열려 있으면 닫기
+            ChoiceBox[1].SetActive(false);
+            ChoiceBox[0].SetActive(true);
+        }
+        else if (ChoiceBox[2].activeSelf)
+        {
+            ChoiceBox[2].SetActive(false);
+            ChoiceBox[0].SetActive(true);
+        }
+    }
+}
