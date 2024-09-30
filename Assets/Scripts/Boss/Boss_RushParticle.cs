26d2e298465c281b081c693ff713befbb46dc780
KoJaeYeon <kojy2004@yonsei.ac.kr>
2024-09-30 10:39:50 +0900

    [build] 기존 XR 패키지 삭제

--- Assets/Scripts/Boss/Boss_RushParticle.cs
+++ Assets/Scripts/Boss/Boss_RushParticle.cs
@@ -1,7 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit.AffordanceSystem.Receiver.Primitives;
 
 public class Boss_RushParticle : MonoBehaviour
 {
